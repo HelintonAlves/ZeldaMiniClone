579b92cc0a32f1b807548c6042825d6679d7ef62
HelintonAlves <helinton-alves@hotmail.com>
2026-02-01 13:09:10 -0300

    Sistema de tiro adicionado

--- src/zeldaminiclone/Player.java
+++ src/zeldaminiclone/Player.java
@@ -1,6 +1,8 @@
 package zeldaminiclone;
 
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Player extends Rectangle{
 
@@ -8,6 +10,9 @@ public class Player extends Rectangle{
     public boolean right, up, down, left;
     public int curAnimation = 0;
     public int curFrames = 0, targetFrames = 15;
+    public static List<Bullet> bullets = new ArrayList<Bullet>();
+    public boolean shoot = false;
+    public int dir = 1;
 
     public Player(int x, int y){
         super(x, y, 32, 32);
@@ -19,9 +24,11 @@ public class Player extends Rectangle{
         if(right && World.isFree(x+spd, y)){
             x += spd;
             moved = true;
+            dir = 1;
         }else if(left && World.isFree(x-spd, y)){
             x -= spd;
             moved = true;
+            dir = -1;
         }
         if(up && World.isFree(x,y-spd)){
             y -= spd;
@@ -40,12 +47,24 @@ public class Player extends Rectangle{
                 }
             }
         }
+        if (shoot){
+            shoot = false;
+            bullets.add(new Bullet(x,y,dir));
+        }
+        for(int i = 0; i < bullets.size(); i++){
+            bullets.get(i).tick();
+        }
+
     }
 
     public void render(Graphics g){
         //g.setColor(Color.blue);
         //g.fillRect(x,y,width,height);
         g.drawImage(SpriteSheet.player_sprite[curAnimation], x,y,32,32,null);
+
+        for(int i = 0; i < bullets.size(); i++){
+            bullets.get(i).render(g);
+        }
 
     }
 }
